038daf62d82278544dd4a5d5a27a014f68d18bf6
Joao Vitor <8ballpoll.fake5@gmail.com>
2025-06-01 23:02:44 -0300

    feat: adiciona botão de logout na navbar

--- resources/views/layouts/navbar.blade.php
+++ resources/views/layouts/navbar.blade.php
@@ -30,6 +30,10 @@
                 </li>
                 {{ @Auth::user()->name }}
             </ul>
+            <form action="{{ route('logout') }}" method="POST">
+                @csrf
+                <button class="btn btn-warning me-5" type="submit">Logout</button>
+            </form>
             <form class="d-flex" role="search">
                 <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
                 <button class="btn btn-outline-success" type="submit">Search</button>
